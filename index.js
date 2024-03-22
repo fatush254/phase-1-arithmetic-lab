989ba066dbc2eae1370139c19b02039961305326
fatush254 <fgalgalo945@gmail.com>
2024-03-22 15:48:08 +0300

    complete task

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
 // Write your code here
+// question 1
 let num1 = 2
 let num2 = 31
 let multiply = num1 * num2
-
+//question 2
 let random;
 do {
     random = Math.floor(Math.random() * 100)
 } while (random=== 0)
-
+//question 3
 let num3 = 20
 let num4 = 16
 let mod = num3 % num4
-
+//question 4
 let max = Math.max(num3, num4)
 let highest = num3
